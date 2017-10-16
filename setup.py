3e36ccdcf3dc26214be315bef2a31f0afc654d4b
nvzard <nitanshu.vzard@gmail.com>
2017-10-17 00:41:20 +0530

    Updated version from 0.2.1 to 0.2.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
       name='rtfd-cli',
-      version='0.2.1',
+      version='0.2.2',
       description='CLI to download docs from ReadTheDocs.org',
       author='Nitanshu Vashistha',
       author_email='nitanshu.vzard@gmail.com',
